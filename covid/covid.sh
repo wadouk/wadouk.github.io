cd9ab0dac44bbcbba2cb1db3884c7e6ca2c7b8f5
Nicolas Bétheuil <wadouk@gmail.com>
2020-03-29 09:28:59 +0200

    update

--- covid/covid.sh
+++ covid/covid.sh
@@ -2,5 +2,3 @@ curl -L https://www.data.gouv.fr/fr/datasets/r/f4935ed4-7a88-44e4-8f8a-33910a151
 sqlite3 -csv -batch covid.db "drop table if exists covid" ".import covid.csv covid" ".header on" ".once pctgrowcovid.csv" "select * from pctgrowavg7 where pays = 'France' and deces >= 10 order by Date"
 git commit -am update
 git push
-
-sqlite3 -csv -batch covid.db "drop table if exists covid" ".import covid.csv covid" ".header on" ".once pctgrowcovid.csv" "select * from pctgrowavg7 where deces >= 10 order by Date"
