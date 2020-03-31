5ea9d8d8cc96fafb1b9b4a75ad1234bbb50a1a78
Nicolas Bétheuil <wadouk@gmail.com>
2020-03-31 08:18:55 +0100

    update

--- covid/covid.sh
+++ covid/covid.sh
@@ -1,3 +1,4 @@
+git pull --rebase
 curl -L https://www.data.gouv.fr/fr/datasets/r/f4935ed4-7a88-44e4-8f8a-33910a151d42 | tail -n +4 | sed 's/;/,/g'> covid.csv
 sqlite3 -csv -batch covid.db "drop table if exists covid" ".import covid.csv covid" ".header on" ".once pctgrowcovid.csv" "select * from pctgrowavg7 where deces >= 10 order by Date"
 git commit -am update
