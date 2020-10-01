3334a5a710b86a611513bebcea60258547debd1d
Nicolas Bétheuil <wadouk@gmail.com>
2020-10-01 16:21:35 +0200

    change url of data

--- covid/covid.sh
+++ covid/covid.sh
@@ -1,5 +1,6 @@
 git pull --rebase
-curl -L https://www.data.gouv.fr/fr/datasets/r/f4935ed4-7a88-44e4-8f8a-33910a151d42 | tail -n +4 | sed 's/;/,/g'> covid.csv
+# https://www.data.gouv.fr/fr/datasets/coronavirus-covid19-evolution-par-pays-et-dans-le-monde-maj-quotidienne/
+curl -L https://www.data.gouv.fr/fr/datasets/r/f4935ed4-7a88-44e4-8f8a-33910a151d42
 sqlite3 -csv -batch covid.db "drop table if exists covid" ".import covid.csv covid" ".header on" ".once pctgrowcovid.csv" "select * from pctgrowavg7 where deces >= 10 and Date <> date('now') order by Date"
 git commit -am update
 git push
