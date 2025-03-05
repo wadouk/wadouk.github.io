bf76e9c96d06256d2c880c2919958aee5867bfbd
Nicolas Bétheuil <wadouk@gmail.com>
2025-03-05 16:38:58 +0100

    diff date

---
+++ jsonresume-theme-stackoverflow-main/theme/hbs-helpers/date-diff.js
@@ -0,0 +1,12 @@
+import { intervalToDuration,  formatDuration, parseISO, interval} from 'date-fns';
+import { fr } from 'date-fns/locale'
+
+const dateDiff = (d1, d2) => {
+  let parseISO2 = d2 === 'now' ? new Date() : parseISO(d2);
+  let parseISO1 = parseISO(d1);
+  let {years, months} = intervalToDuration(interval(parseISO1, parseISO2));
+  let f = formatDuration({years, months}, {locale: fr})
+  return `(${f})`
+}
+
+export { dateDiff };
